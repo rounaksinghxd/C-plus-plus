b048175ae74d6f97cd53661314eafa8e78e34027
rounaksinghxd <singhrounak00326@gmail.com>
2026-07-20 00:27:07 +0530

    Define some data type

--- basics.cpp
+++ basics.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
-using namespace std;
+using namespace std; //we use this line of code because after this code we didn't have to use std in cout or cin.
+
 int main()
 
 {
-    int first_number, second_number, sum;
+    int first_number, second_number, sum; // It is a type of data type which is used to store whole number without a decimal.
 
-    cout << "Enter two integers: ";
-    cin >> first_number >> second_number;
+    cout << "Enter two integers: "; // It is used to display the output
+    cin >> first_number >> second_number; //It is used to store the value.
 
     sum = first_number + second_number;
 
