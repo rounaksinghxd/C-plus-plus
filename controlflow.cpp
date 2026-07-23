2c461bb7882e63008405084fc1c32f3e54a0e559
rounaksinghxd <singhrounak00326@gmail.com>
2026-07-24 00:46:02 +0530

    Defined Conditional Statements

--- controlflow.cpp
+++ controlflow.cpp
@@ -13,3 +13,20 @@ int main()
     int sum = x+y; //Executes strictly after x and y are declared.
     cout << sum;
 }
+/*
+2) Selection Statements (Conditionals)
+These statements allow your code to make decisions and execute specific blocks depending on whether a condition is true or false.
+
+These statements we already learned in Python and C Programming Language.
+
+if statement: Executes a block of code only if its condition evaluates to true.
+
+if-else statement: Provides an alternative block of code to run if the condition evaluates to false.
+
+else if ladder: Evaluates multiple sequential conditions until one evaluates to true.
+
+switch statement: Tests a single variable against multiple constant values (case), replacing long if-else ladders for multi-way branching.
+
+Ternary operator (? :): Acts as a compact, inline single-line replacement for basic if-else assignments.
+
+*/
