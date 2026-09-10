002832d80c1463bded4923a3056fcc991064e695
rounaksinghxd <singhrounak00326@gmail.com>
2026-09-10 16:45:54 +0530

    Example of functions

--- functions.cpp
+++ functions.cpp
@@ -7,3 +7,13 @@ They help break down large programs into smaller, manageable pieces, preventing 
 void Functions: Return no value at all.
 int Functions: Return some value exactly (one integer).
 */
+#include <iostream>
+using namespace std;
+void greet() {
+    cout <<"Hello Rounak"<<endl;
+}
+int main() {
+    greet(); // this line of code used to call the function.
+    greet(); // we can print multiple times this using only call in functions.
+    return 0;
+}
