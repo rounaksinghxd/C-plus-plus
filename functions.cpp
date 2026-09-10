81d7c1b870a8d421609ca023a700800338d08b7f
rounaksinghxd <singhrounak00326@gmail.com>
2026-09-10 16:41:27 +0530

    Define function and difference between void and int type of Functions

---
+++ functions.cpp
@@ -0,0 +1,9 @@
+// In this repo we learn about Functions in C++
+/*
+Functions is a: Reusable blocks of code that perform a specific task. 
+They help break down large programs into smaller, manageable pieces, preventing you from writing the same code multiple times.
+
+// Difference between Void and int Functions:
+void Functions: Return no value at all.
+int Functions: Return some value exactly (one integer).
+*/
