8694e5f66b0651db2abfb52aa268207506820d5a
rounaksinghxd <singhrounak00326@gmail.com>
2026-09-11 18:12:53 +0530

    Used functions to greet and value together

--- functions.cpp
+++ functions.cpp
@@ -9,11 +9,28 @@ int Functions: Return some value exactly (one integer).
 */
 #include <iostream>
 using namespace std;
-void greet() {
+// function defination
+/*void greet() {
     cout <<"Hello Rounak"<<endl;
 }
 int main() {
     greet(); // this line of code used to call the function.
-    greet(); // we can print multiple times this using only call in functions.
+    greet(); // we can print multiple times this using only by calling the function multiple times.
     return 0;
 }
+    */
+// Now let's say we want a return value from function so in that case we have to void function to int function.
+int greets() {
+    cout <<"Hello World" <<endl;
+    return 100;
+}
+int main() {
+    int value = greets();
+    cout<<"Value is: "<<value <<endl ;
+    return 0;
+}
+
+
+
+
+
